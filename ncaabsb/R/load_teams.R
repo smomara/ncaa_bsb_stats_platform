30ecd9b552e9f0b769ce1482ca26afc7e1e2e7b0
smomara <omarams@cua.edu>
2024-04-08 13:11:58 -0400

    Teams Data Fix
    fixed data in teams.csv and updates the load_teams functoin to reflect that

--- ncaabsb/R/load_teams.R
+++ ncaabsb/R/load_teams.R
@@ -14,7 +14,7 @@
 #' @importFrom dplyr filter
 #' @importFrom baseballr load_ncaa_baseball_teams
 load_teams <- function() {
-  teams <- baseballr::load_ncaa_baseball_teams() %>% 
-    dplyr::filter(year == 2024)
+  url <- "https://raw.githubusercontent.com/smomara/ncaa_bsb_stats_platform/main/ncaabsb/data/guts.csv"
+  teams <- read.csv(url)
   teams
 }
